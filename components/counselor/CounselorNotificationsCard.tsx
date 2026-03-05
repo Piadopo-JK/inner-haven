477a921f757c49aa2242a6828a2c987a8465f6d6
Kissu1 <ricoposas@gmail.com>
2026-03-06 00:42:53 +0800

    Add counselor dashboard
    -add counselor specific components

---
+++ components/counselor/CounselorNotificationsCard.tsx
@@ -0,0 +1,11 @@
+import { DashboardCard } from "@/components/dashboard/DashboardCard";
+import { Md3Message } from "@/components/ui/md3-message";
+
+export default function CounselorNotificationsCard() {
+
+  return (
+    <DashboardCard title="Notifications">
+      <Md3Message tone="info">Notifications card placeholder (UR1).</Md3Message>
+    </DashboardCard>
+  );
+}
